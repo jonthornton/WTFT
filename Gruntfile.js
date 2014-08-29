20f96d6abab52e958c7fa530ef0cf6c7239b4d25
Jon Thornton <thornton.jon@gmail.com>
2014-08-29 14:51:27 -0400

    Cleaned up indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,40 @@
 module.exports = function(grunt) {
-	 grunt.initConfig({
+	grunt.initConfig({
 
-		less: {
-			development: {
-				files: {"dev/styles.css": "styles.less"}
+	less: {
+		development: {
+			files: {"dev/styles.css": "styles.less"}
+		},
+		production: {
+			options: {
+				cleancss: true
 			},
-			production: {
-				options: {
-					cleancss: true
-				},
-				files: {"dist/styles.css": "styles.less"}
+			files: {"dist/styles.css": "styles.less"}
+		}
+	},
+	uglify: {
+		options: {
+			report: 'gzip'
+		},
+		dist: {
+			files: {
+			'dist/fastclick.min.js': 'bower_components/fastclick/lib/fastclick.js',
 			}
-		},
-        uglify: {
-            options: {
-                report: 'gzip'
-            },
-            dist: {
-                files: {
-                    'dist/fastclick.min.js': 'bower_components/fastclick/lib/fastclick.js',
-                }
-            }
-        },
-		watch: {
-			scripts: {
-				files: '**/*.less',
-				tasks: ['less'],
-				options: {
-					debounceDelay: 250,
-				},
+		}
+	},
+	watch: {
+		scripts: {
+			files: '**/*.less',
+			tasks: ['less'],
+			options: {
+				debounceDelay: 250,
 			},
 		},
-	 });
-     grunt.loadNpmTasks('grunt-contrib-uglify');
-	 grunt.loadNpmTasks('grunt-contrib-less');
-	 grunt.loadNpmTasks('grunt-contrib-watch');
+	},
+	});
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	 grunt.registerTask('default', ['less', 'uglify']);
- };
+	grunt.registerTask('default', ['less', 'uglify']);
+};
